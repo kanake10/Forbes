4be0716f05e2c0b1bc90989a52ab2e3da14da565
kanake10 <10kanake@gmail.com>
2022-09-13 09:54:32 +0300

    forbes app

---
+++ core/src/main/java/com/example/core/Constants.kt
@@ -0,0 +1,16 @@
+package com.example.core
+
+object Constants {
+    const val BASE_URL = "https://forbes400.herokuapp.com/api/"
+    const val FORBES_DB = "forbes_db"
+    const val ALL_FORBES_ENDPOINT = "forbes400"
+    const val OLDEST_ENDPOINT = "forbes400/oldest"
+    const val YOUNGEST_ENDPOINT = "forbes400/youngest"
+    const val MALE_ENDPOINT = "forbes400/youngest"
+    const val FEMALE_ENDPOINT = "forbes400/youngest"
+    const val INDUSTRY_ENDPOINT = "forbes400/industries/retail"
+}
+
+/**
+ * other industries to filter include (fashion,finance,investments)
+ */

c4e35d32e3cdbe4b2e206fe0c1cf6c19d976c15d
kanake10 <10kanake@gmail.com>
2022-09-14 21:58:11 +0300

    cache module

---
+++ layers/cache/src/main/java/com/example/cache/dao/AllForbesDao.kt
@@ -0,0 +1,7 @@
+package com.example.cache.dao
+
+import androidx.room.Dao
+
+@Dao
+interface AllForbesDao {
+}
--- layers/cache/src/main/java/com/example/cache/entity/AllForbesEntity.kt
+++ layers/cache/src/main/java/com/example/cache/entity/AllForbesEntity.kt
@@ -1,3 +1,17 @@
 package com.example.cache.entity
 
-data class AllForbesEntity()
+import androidx.room.Entity
+
+@Entity
+data class AllForbesEntity(
+    val rank: Int,
+    val finalWorth: Double,
+    val personName: String,
+    val city: String,
+    val source: String,
+    val industries: List<String>,
+    val countryOfCitizenship: String,
+    val squareImage: String,
+    val bios: List<String>,
+    val abouts: List<String>
+)

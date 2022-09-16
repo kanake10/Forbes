bd6eb613e8eee50f7ded95aa96ec0d35a615e6a4
kanake10 <10kanake@gmail.com>
2022-09-16 22:18:50 +0300

    dto to entity mapper

---
+++ layers/data/src/main/java/com/example/data/mappers/DtoToEntity.kt
@@ -0,0 +1,94 @@
+package com.example.data.mappers
+
+import com.example.cache.entity.*
+import com.example.data.dtos.*
+
+internal fun AllForbesDto.toEntity():AllForbesEntity{
+    return AllForbesEntity(
+        this.rank,
+        this.finalWorth,
+        this.personName,
+        this.city,
+        this.source,
+        this.industries,
+        this.squareImage,
+        this.countryOfCitizenship,
+        this.bios,
+        this.abouts
+    )
+}
+
+internal fun FemaleDto.toEntity():FemalesEntity{
+    return FemalesEntity(
+        this.rank,
+        this.finalWorth,
+        this.personName,
+        this.city,
+        this.source,
+        this.industries,
+        this.squareImage,
+        this.countryOfCitizenship,
+        this.bios,
+        this.abouts
+    )
+}
+
+internal fun IndustryDto.toEntity():AccordingToIndustryEntity{
+    return AccordingToIndustryEntity(
+        this.rank,
+        this.finalWorth,
+        this.personName,
+        this.city,
+        this.source,
+        this.industries,
+        this.squareImage,
+        this.countryOfCitizenship,
+        this.bios,
+        this.abouts
+    )
+}
+
+internal fun MalesDto.toEntity():MalesEntity{
+    return MalesEntity(
+        this.rank,
+        this.finalWorth,
+        this.personName,
+        this.city,
+        this.source,
+        this.industries,
+        this.squareImage,
+        this.countryOfCitizenship,
+        this.bios,
+        this.abouts
+    )
+}
+
+internal fun OldestDto.toEntity():OldestEntity{
+    return OldestEntity(
+        this.rank,
+        this.finalWorth,
+        this.personName,
+        this.city,
+        this.source,
+        this.industries,
+        this.squareImage,
+        this.countryOfCitizenship,
+        this.bios,
+        this.abouts
+    )
+}
+
+internal fun YoungestDto.toEntity():YoungestEntity{
+    return YoungestEntity(
+        this.rank,
+        this.finalWorth,
+        this.personName,
+        this.city,
+        this.source,
+        this.industries,
+        this.squareImage,
+        this.countryOfCitizenship,
+        this.bios,
+        this.abouts
+    )
+}

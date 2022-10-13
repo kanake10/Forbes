a4afb834364c0ddddb34bb103b17814434286b9a
kanake10 <10kanake@gmail.com>
2022-10-13 03:47:09 +0300

    room migration to v4

--- layers/cache/src/main/java/com/example/cache/db/ForbesDatabase.kt
+++ layers/cache/src/main/java/com/example/cache/db/ForbesDatabase.kt
@@ -1,19 +1,34 @@
 package com.example.cache.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.cache.converters.Converters
 import com.example.cache.dao.*
 import com.example.cache.entity.*
+import com.example.core.Constants.FORBES_DB
 
 @TypeConverters(Converters::class)
 @Database(
     entities = [AccordingToIndustryEntity::class,AllForbesEntity::class,FemalesEntity::class,MalesEntity::class,OldestEntity::class,YoungestEntity::class],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class ForbesDatabase :RoomDatabase(){
+
+    companion object {
+        fun getInstance(context: Context): ForbesDatabase {
+            return Room.databaseBuilder(context, ForbesDatabase::class.java, FORBES_DB)
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
+
+
+
+
     abstract fun AllForbesDao():AllForbesDao
     abstract fun OldestDao():OldestDao
     abstract fun MalesDao():MalesDao
